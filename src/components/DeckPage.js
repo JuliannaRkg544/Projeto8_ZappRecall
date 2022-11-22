a104828d2bec3fe10a8416fc7788416ff7549f51
JulianaRkg544 <julianna.rkg544@gmail.com>
2022-11-22 15:08:37 -0300

    more questions

--- src/components/DeckPage.js
+++ src/components/DeckPage.js
@@ -10,10 +10,10 @@ const flashcards = [
     { key: 2, question: "Dê exemplo de um Sistema Distribuído e justifique", answer: "Qualquer aplicação mobile pode servir de exemplo, como youtube e facebook, pq elas podem ser executadas em diferentes dispositvos e permitir a comunicação entre estes " },
     { key: 3, question: "Cite alguns desafios comuns a implementação de SD's", answer: "Segurança, Escalabilidade, Desempenho, Tolerância a falhas e Comunicação entre os componentes, " },
     { key: 4, question: "A internet poide ser considerada um SD? Pq? ", answer: "Sim, a internet é o maior exemplo de sistema distribuído; ela provê serviços que estão localizados em máquinas dispersas que são interligadas por algum tipo de rede, redes essas que utilizam de um meio de comunicação comum, os protocolos." },
-    // { key: 5, question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma" },
-    // { key: 6, question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências" },
-    // { key: 7, question: "Usamos props para __", answer: "passar diferentes informações para componentes" },
-    // { key: 8, question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+    { key: 5, question:"Qual a função de um middleware em SD's?", answer:"Num SD os middleawares são responsáveis por permitir que aplicações diferentes funcionem em máquinas difrentes. É uma camada de software que vai atuar entre a camada de aplicação e o SO "},
+    { key: 6, question: "Explique o que é arquitetura descentralizada, cite um exemplo desta arquitetura e reflita sobre uma vantagem e uma desvantagem desses sistema", answer: "É um tipo de arquitetura de sistemas onde não há uma definição estática (central) de cliente e servidor. Como exemplo temos o P2P, um modelo de arquitetura onde cada um dos pontos ou nós da rede funciona tanto como cliente quanto como servidor" },
+    { key: 7, question: "Como podemos dividir uma arquitetura centralizada a nível de camadas?", answer: "É necessário minimamente um nível de interface com usuário, processamento e dados" },
+    { key: 8, question: "Qual a relação entre arquitetura centralizada e multidividida?", answer: "arquitetura multidividida é um tipo de arquitetura centralizada que se subdivide quanto ao nível de implementação feito pelo cliente ou servidor.Ela pode ser do tipo Fat quando o cliente é responsável por realizar mais processos ou do tipo Thin quando o servidor possui maior peso na aplicação " }
 ]
 
 
